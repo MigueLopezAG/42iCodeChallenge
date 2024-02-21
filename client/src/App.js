031bc45b86607ad8e71e866f32d4a89760a745ce
Miguel Angel Lopez Gudiño <miguel.lopez@magnotechnology.com>
2024-02-21 00:58:28 -0600

    create client, server and connect them with docker

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react'; 
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [message, setMessage] = useState("");
+  
+  useEffect (() => {
+    fetch("http://localhost:3001")
+    .then(res => res.json())
+    .then(data => setMessage (data.message))
+    .catch(err => console.log(err));
+  }, []);
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {message}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
